b3a9306e23947d73b54a1fa91019d8276e17e79a
davidzyc <davidzyc2000@hotmail.com>
2019-03-16 19:57:17 +0800

    Route API Adaptation

--- routes/web.php
+++ routes/web.php
@@ -21,24 +21,29 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 // Route::resource('/team', 'TeamController');
 // General Route
-Route::get('/init', 'TodoController@init');
-Route::get('/team/my', 'TeamController@getMyTeams');
-Route::get('/team/{team}/members', 'TeamController@getMembers');
-Route::get('/team/{team}/todos', 'TodoController@getTodosFromTeam');
-Route::post('/team/{team}/todo/{todo}/reply', 'ReplyController@create');
-Route::get('/file', 'FileController@getMyFiles');
-Route::get('/file/{file}', 'FileController@getMySpecificFile');
-Route::post('/file', 'FileController@create');
-Route::get('/team/{team}/file/{file}', 'FileController@getFileFromTeam');
-Route::post('/reply/{reply}/file/{file}/toggle', 'FileController@toggleFileToReply');
 
-// Admin Route
-Route::get('/team/{team}/todo/{todo}/reply', 'TodoController@getRepliesFromTodo');
-Route::get('/team/{team}/member/{user}/toggle', 'TeamController@toggleMember');
-Route::post('/team/{team}/todos', 'TodoController@create');
-Route::post('/team', 'TeamController@store');
-Route::post('/team/{team}/todo/edit', 'TodoController@editTodo');
-Route::post('/team/{team}/file/{file}', 'FileController@toggleShareFileWithTeam');
-Route::get('/todo/{todo}/file/{file}', 'FileController@getFileFromTodo');
-Route::get('/todo/{todo}/replies', 'ReplyController@getRepliesFromTodo');
+Route::prefix('api/v1')->middleware('auth')->group(function () {
+    
+    Route::get('/init', 'TodoController@init');
+    Route::get('/team/my', 'TeamController@getMyTeams');
+    Route::get('/team/{team}/members', 'TeamController@getMembers');
+    Route::get('/team/{team}/todos', 'TodoController@getTodosFromTeam');
+    Route::post('/team/{team}/todo/{todo}/reply', 'ReplyController@create');
+    Route::get('/file', 'FileController@getMyFiles');
+    Route::get('/file/{file}', 'FileController@getMySpecificFile');
+    Route::post('/file', 'FileController@create');
+    Route::get('/team/{team}/file/{file}', 'FileController@getFileFromTeam');
+    Route::post('/reply/{reply}/file/{file}/toggle', 'FileController@toggleFileToReply');
+    
+    // Admin Route
+    Route::get('/team/{team}/todo/{todo}/reply', 'TodoController@getRepliesFromTodo');
+    Route::get('/team/{team}/member/{user}/toggle', 'TeamController@toggleMember');
+    Route::post('/team/{team}/todos', 'TodoController@create');
+    Route::post('/team', 'TeamController@store');
+    Route::post('/team/{team}/todo/edit', 'TodoController@editTodo');
+    Route::post('/team/{team}/file/{file}', 'FileController@toggleShareFileWithTeam');
+    Route::get('/todo/{todo}/file/{file}', 'FileController@getFileFromTodo');
+    Route::get('/todo/{todo}/replies', 'ReplyController@getRepliesFromTodo');
+
+});    
 
